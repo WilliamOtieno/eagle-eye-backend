f529de7a4752174a180c2cc9d679285741e21800
WilliamOtieno <jimmywilliamotieno@gmail.com>
2023-04-01 14:47:05 +0300

    fix

--- core/tasks.py
+++ core/tasks.py
@@ -6,6 +6,6 @@ from .services import generate_image_from_chunks
 
 @shared_task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 5})
 def process_payload():
-    data_ = PicData.objects.filter(is_processed=False, is_full=True)
+    data_ = PicData.objects.filter(is_processed=False, is_full=True, is_ready=True)
     for data in data_:
         generate_image_from_chunks(chunk_id=data.chunk_id)
