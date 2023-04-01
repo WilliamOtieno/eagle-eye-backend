d52548d23c19af31c6d56f818e34a6899f8b233d
WilliamOtieno <jimmywilliamotieno@gmail.com>
2023-04-01 08:08:54 +0300

    fix

--- core/views.py
+++ core/views.py
@@ -11,22 +11,18 @@ from .models import TempData, PicData
 class PicDataView(View):
 
     def get(self, request, *args, **kwargs):
-        print('GET Request:......')
-        print(self.request.GET)
-        print('GET Request:......')
         return HttpResponse('GET method ok')
 
     def post(self, request, *args, **kwargs):
         d = self.request.body.decode('utf-8')
         data = json.loads(d)
-        print(data)
         if data.get('id') is not None:
             temp = TempData.objects.create(
                 chunk_id=data.get('id'),
                 index=data.get('index'),
                 payload=data
             )
-            permanent, _ = PicData.objects.get_or_create(chunk_id=temp.chunk_id, chip=temp.chip)
+            PicData.objects.get_or_create(chunk_id=temp.chunk_id, chip=temp.chip)
         else:
             print('Empty Data Received')
         return HttpResponse('ok')
