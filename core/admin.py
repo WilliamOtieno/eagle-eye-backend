101a9aa3867fb90dbc7da6802241ead26591eb91
williamotieno <jimmywilliamotieno@gmail.com>
2023-04-19 15:28:15 +0300

    patch

--- core/admin.py
+++ core/admin.py
@@ -4,8 +4,8 @@ from .models import PicData, TempData
 
 @admin.register(PicData)
 class PicDataAdmin(admin.ModelAdmin):
-    list_display = ['pic_id', 'chip', 'is_full', 'is_ready', 'is_processed']
-    list_filter = ['pic_id', 'chip', 'is_full', 'is_ready', 'is_processed']
+    list_display = ['pic_id', 'chip', 'is_full', 'is_ready', 'is_processed', 'created', 'updated']
+    list_filter = ['pic_id', 'chip', 'is_full', 'is_ready', 'is_processed', 'created', 'updated']
     search_fields = ['pic_id', 'chip']
 
 
