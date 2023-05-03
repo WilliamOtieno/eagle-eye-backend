714856acab7bfab362dcca00991925f2730b026f
williamotieno <jimmywilliamotieno@gmail.com>
2023-05-03 04:28:27 +0000

    fix

--- EagleEye/settings.py
+++ EagleEye/settings.py
@@ -163,6 +163,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'https://9a44-105-163-2-130.ngrok-free.app',
     'http://35.226.222.242',
+    'http://206.189.48.24',
 ]
 
 SERVERLESS_CRONJOBS = [
